a8418a28e8f95059ab5b7a06fb152807e5d68ab6
samiatrz <tourriz.samia@gmail.com>
2021-05-23 16:31:23 +0100

    progress bar s

---
+++ progress_samia.py
@@ -0,0 +1,8 @@
+from tqdm import tqdm
+from time import sleep
+
+for i in tqdm(range(0,100),desc="Loading : "):
+    sleep(0.1)
+
+for i in tqdm(range(0,100),desc="Loading : ",ascii="123456789♦"):
+    sleep(0.1)
